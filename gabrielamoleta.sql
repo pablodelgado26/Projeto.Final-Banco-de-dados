5ee74aa5046bca0688bd3d366a307e5341572bbb
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2024-11-12 11:32:52 -0300

    feat: create table dicas

--- gabrielamoleta.sql
+++ gabrielamoleta.sql
@@ -2,5 +2,13 @@ CREATE TABLE carreiras (
     id_carreira SERIAL PRIMARY KEY,
     profissao VARCHAR(150) NOT NULL,
     requisitos TEXT NOT NULL,
-    salario DECIMAL(10, 2)
+    salario DECIMAL(10, 2),
+    imagem_carreiras BLOB
+);
+
+CREATE TABLE dicas (
+    id_dicas SERIAL PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    descricao TEXT NOT NULL,
+    imagem_dicas BLOB
 );
